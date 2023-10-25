7b5832ffbb6367c40d862e5dde47f2462754cdae
JacobKleim <yakovkleimenov@yandex.ru>
2023-10-26 01:39:01 +0400

    Добавлены функция и класс для тестирования нового функционала

--- projects_automation/form/form.py
+++ projects_automation/form/form.py
@@ -1,11 +1,48 @@
 from django import forms
-
+from datetime import datetime, timedelta
+from datetime import time
 from pr_auto.models import Student
+
+
+# available_time = ['10:00-12:00', '20:00-22:00', ]   #PM.objects.values_list('available_time', flat=True)
+# timings_list = []
+
+# def count_timings(available_time, timings_list):
+#     timings_list = []
+#     for t in available_time:
+#         timings = []
+#         start_time, end_time = t.split('-')
+#         start = datetime.strptime(start_time, "%H:%M")
+#         print(start.strftime("%H:%M"))
+#         end = datetime.strptime(end_time, "%H:%M")
+#         result = end - start
+#         print(result)
+#         result2 = result / timedelta(minutes=30)
+#         print(result2)
+#         for _ in range(int(result2)):
+#             if not timings:
+#                 tm = start + timedelta(minutes=30)
+#                 time_str = str(tm.strftime("%H:%M"))
+#                 timings.append(f"{start.strftime('%H:%M')}-{time_str}")
+#             else:    
+#                 previous_time = timings[-1].split('-')[1]
+#                 start = datetime.strptime(previous_time, "%H:%M")
+                
+#                 tm = start + timedelta(minutes=30) 
+#                 time_str = str(tm.strftime("%H:%M"))
+#                 timings.append(f"{previous_time}-{time_str}")
+#         timings_list.append(timings)    
+#     return timings_list
+#     # return timings
+
+
+# print(count_timings(available_time, timings_list)) 
 
 
 
 class Project_Registration_Form(forms.ModelForm):
-    # call_time = 
+    # available_time = PM.objects.values_list('available_time', flat=True)
+
     class Meta:
         model = Student
-        fields = ('email',)
+        fields = ('email', )
--- projects_automation/pr_auto/models.py
+++ projects_automation/pr_auto/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator
 
 
 class Student(models.Model):
@@ -33,34 +33,53 @@ class Student(models.Model):
         'Удобное время для созвона',
         max_length=50,
         db_index=True,
-        blank=True)
-    
+        blank=True)#переименовать в 'удобное время'
+
     is_dv = models.BooleanField('Ученик с Дальнего Востока', default=None,
                                 null=True, blank=True)
-    
-    registration_status = models.BooleanField('Статус регистрации в проекте', default=False)
-    
 
-    
+    registration_status = models.BooleanField('Статус регистрации в проекте', default=False)
+
     class Meta:
         verbose_name = 'Student'
         verbose_name_plural = ' Students'
         constraints = [
             models.UniqueConstraint(
                 fields=['name', 'email', 'telegram_id'],
+                name='unique_name_email_telegram_id'
             )]
-
 
     def __str__(self):
         return f'{self.name}'
+
+
+# class Call_time(models.Model):
+#     time = models.TimeField(auto_now=False, auto_now_add=False)
+#     participants_number = models.PositiveIntegerField(
+#         'Время созвона',
+#         validators=[
+#             MaxValueValidator(
+#                 5)]
+#         )
+#     product_manager = models.ForeignKey("PM", verbose_name=("менеджер проекта"), on_delete=models.CASCADE)
+#     group = models.ForeignKey('Group', verbose_name=("Группа"), on_delete=models.CASCADE)
+
+
+
+class Group(models.Model):
+    pass
+
     
+
+
+
 
 # class PM(models.Model):
 #     name = models.CharField(
 #         'ФИО продукт менеджера',
 #         max_length=50,
 #         db_index=True)
-#     available_time = models.TimeField()
+#     available_time = models.TimeField()#переименовать в 'удобное время'
 
 
 # class Enrolled_student(models.Model):
